b640033125ff2b1d7199a4eb4cfd278553934fe5
prathamesh <prathameshsk94@gmail.com>
2024-03-11 22:18:07 +0530

    mongodb connection configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import connectDB from "./db/index.js";
 
 const app = express();
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log("app started on port 8000");
-});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("app started on port 8000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
